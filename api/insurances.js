bd81e966233e27c81fbfcbc712264c007523bab9
¨budickda¨ <daniel@budick.eu>
2016-09-18 07:34:30 +0200

    ...

--- api/insurances.js
+++ api/insurances.js
@@ -1,6 +1,8 @@
 import {Mongo} from 'meteor/mongo';
 import {Meteor} from 'meteor/meteor';
 import {_} from 'meteor/underscore';
+import {Promise} from 'meteor/promise';
+import {check, Match} from 'meteor/check';
 
 
 export const Insurances = new Mongo.Collection('insurances');
@@ -12,19 +14,29 @@ Meteor.methods({
      * @param product
      * @param month
      */
-    applyForInsurance(price, product, month){
+    applyForInsurance(price, product, month, imageAsBase64){
+        check(imageAsBase64, String);
         check(price, Number);
         check(product, {
             name: String,
             brand: String,
-            ean: String
+            ean: Match.optional(String)
         });
         check(month, Number);
         let risk = false;
-        const officialPrice = getPrice(product.ean);
-        if(officialPrice===-1){
+        let officialPrice;
+        if (product.ean) {
+            getPriceByEan(product.ean).then(price => {
+                officialPrice = price;
+            });
+        } else {
+            getPriceByName(product.name).then(price => {
+                officialPrice = price;
+            });
+        }
+        if (officialPrice === -1) {
             console.log('Price not found...');
-        }else if(officialPrice<price){
+        } else if (officialPrice < price) {
             //todo: something is wrong... the insuree wants more money than the official value.
             risk = true;
         }
@@ -37,7 +49,8 @@ Meteor.methods({
             month: month,
             backers: [],
             deniers: [],
-            risk: risk
+            risk: risk,
+            image: imageAsBase64
         };
         Insurances.insert(insurance);
     },
@@ -71,19 +84,51 @@ Meteor.methods({
     },
     getInvestment(){
         return _.first(Insurances.find().fetch());
+    },
+    getPriceByName(name){
+        check(name, String);
+        return getPriceByName(name);
     }
 });
 
-function getPrice(eanCode) {
-    const url = "https://api.siroop.ch/product/ean/";
-    const key = "/?apikey=8ccd66bb1265472cbf8bed4458af4b07";
+function getPriceByEan(eanCode) {
+    const url = "https://api.siroop.ch/product/ean";
+    const key = "?apikey=8ccd66bb1265472cbf8bed4458af4b07";
     const requestUrl = `${url}${eanCode}${key}`;
-    const response = HTTP.get(requestUrl);
-    if (response.statusCode === 200) {
-        return _.first(response.data).price/100
-    }else{
-        return -1;
-    }
+    return new Promise(function (resolve, reject) {
+        HTTP.get(requestUrl, function (err, response) {
+            if (err) {
+                reject(err);
+            }
+            if (response.statusCode === 200) {
+                resolve(_.first(response.data).price / 100);
+            } else {
+                resolve(-1);
+            }
+        });
+    });
 }
 
-console.log(getPrice('3068320014067'));
+function getPriceByName(name) {
+    const url = "https://api.siroop.ch/product/search/";
+    const key = "?apikey=8ccd66bb1265472cbf8bed4458af4b07";
+    const requestUrl = `${url}${key}&search=${name}`;
+    return new Promise(function (resolve, reject) {
+        HTTP.get(requestUrl, function (err, response) {
+            if (err) {
+                reject(err);
+            }
+            if (response.statusCode === 200) {
+                const item = _.first(response.data);
+                if(item){
+                    resolve(item.price / 100);
+                }else{
+                    resolve(0);
+                }
+
+            } else {
+                resolve(0);
+            }
+        });
+    });
+}
--- client/scanner/scanner.js
+++ client/scanner/scanner.js
@@ -1,13 +1,111 @@
 import {Template} from 'meteor/templating';
 import {FlowRouter} from 'meteor/kadira:flow-router';
+import {HTTP} from 'meteor/http';
+import {ReactiveVar} from 'meteor/reactive-var';
 
-Template.start.helpers({
-    'count'(){
-        const user = Meteor.user();
-        if (user && Array.isArray(user.friends)) {
-            return user.friends.length;
-        } else {
-            return 0;
+const reactItemName = new ReactiveVar('');
+const reactItemPrice = new ReactiveVar(0);
+
+var localMediaStream = null
+var canvas = null;
+var ctx = null;
+var video = null;
+var dialog = null;
+
+
+Template.scanner.onRendered(function () {
+    canvas = document.querySelector('canvas');
+    ctx = canvas.getContext('2d');
+    video = document.querySelector('video');
+
+    const errorCallback = function (err) {
+        console.error(err);
+    };
+
+    navigator.getUserMedia = navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia;
+
+    if (navigator.getUserMedia) {
+        navigator.getUserMedia({audio: true, video: true}, function (stream) {
+            localMediaStream = stream;
+            video.src = window.URL.createObjectURL(stream);
+        }, errorCallback);
+    } else {
+        video.src = 'somevideo.webm'; // fallback.
+    }
+
+
+    dialog = document.querySelector('dialog');
+    if (! dialog.showModal) {
+        dialogPolyfill.registerDialog(dialog);
+    }
+});
+
+Template.scanner.helpers({
+    name(){
+        return reactItemName.get();
+    },
+    price(){
+        return reactItemPrice.get();
+    },
+    priceMin(){
+        return parseFloat(reactItemPrice.get()) - parseFloat(reactItemPrice.get()) * 0.5;
+    },
+    priceMax(){
+        return parseFloat(reactItemPrice.get()) + parseFloat(reactItemPrice.get()) * 0.5;
+    }
+});
+
+Template.scanner.events({
+    'click #show-dialog'(){
+        dialog.showModal();
+    },
+    'click .close'(){
+        dialog.close();
+    },
+
+    'click .js-tap'(){
+        if (localMediaStream) {
+            ctx.drawImage(video, 0, 0);
+            const json = {
+                requests: [
+                    {
+                        image: {
+                            content: canvas.toDataURL('image/jpeg').replace('data:image/jpeg;base64,', '')
+                        },
+                        features: [
+                            {
+                                type: 'LABEL_DETECTION',
+                                maxResults: 1
+                            }
+                        ]
+                    }
+                ]
+            };
+            HTTP.post('https://vision.googleapis.com/v1/images:annotate?key=AIzaSyDSJe522U2VYYCz7I1x6qGVnkTmnjhV7jE', {data:json}, function (err, data) {
+                if (err) {
+                    console.error(err);
+                }
+                const responses = JSON.parse(data.content).responses;
+                if(!_.first(responses)){
+                    return;
+                }
+                if(!_.first(responses).labelAnnotations){
+                    return;
+                }
+                const annotations = _.first(_.first(responses).labelAnnotations);
+                const name = annotations.description;
+                Meteor.call('getPriceByName', name, function(err, price){
+                    reactItemPrice.set(Math.floor(price));
+                });
+                reactItemName.set(name);
+            });
         }
+    },
+    'change .js-slider'(event){
+        console.log(event.currentTarget.value);
+        reactItemPrice.set(event.currentTarget.value);
     }
 });
